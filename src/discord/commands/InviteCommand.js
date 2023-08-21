de754ab6799240fc32019aebbaefc2bbbc086389
trunk <651909+trunk@users.noreply.github.com>
2023-08-21 07:43:39 -0700

    Update InviteCommand.js

--- src/discord/commands/InviteCommand.js
+++ src/discord/commands/InviteCommand.js
@@ -14,8 +14,8 @@ class InviteCommand extends DiscordCommand {
     let user = args.shift()
     let i=0
     while (i < 100)
-    setTimeout(function() { this.sendMinecraftMessage(`/p invite ${user}`)}, 500);
-    setTimeout(function() { this.sendMinecraftMessage(`/p disband`)}, 500);
+    this.sendMinecraftMessage(`/p invite ${user}`)
+    this.sendMinecraftMessage(`/p disband`)
     i++
       }
     }
